01fc90b96b97717ef245c471ffaccae5d8ebd07e
ctrl-Sebastian <74610997+ctrl-Sebastian@users.noreply.github.com>
2022-12-03 13:13:26 -0400

    Added all functions
    Can view Restaurants and menus, create new ones, edit and delete

---
+++ restaurants/sql/__init__.py
@@ -0,0 +1,14 @@
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+engine = create_engine('sqlite:///restaurants.db', connect_args={"check_same_thread": False})
+
+
+"Bind"
+DBSession = sessionmaker(bind=engine)
+cursor = DBSession()
+
+from restaurants.sql.models import Restaurant, Menu
