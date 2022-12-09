ffcd11d7886d2eeeeb1ff3b345899bc7000f6462
ctrl-Sebastian <sebastianloslocos@gmail.com>
2022-12-08 21:31:02 -0400

    added json file

--- restaurants/routes.py
+++ restaurants/routes.py
@@ -3,7 +3,26 @@ from restaurants import app
 from restaurants.sql import cursor
 from restaurants.sql.models import Base, Restaurant, Menu
 
+#OAuth imports
+import os
+import pathlib
+import requests
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+from google.oauth2 import id_token
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
 GOOGLE_CLIENT_ID = "610084126582-l8v4hv1clqcphtep9pmv01fd1m031ig5.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(client_secrets_file=client_secrets_file, 
+        scopes=["https://www.googleapis.com/auth/userinfo.profile", 
+        "https://www.googleapis.com/auth/userinfo.email", 
+        "openid"],
+        redirect_uri="http://127.0.0.1:5000/callback"
+        )
 
 def login_is_required(function):
     def wrapper(*args, **kwargs):
@@ -16,12 +35,28 @@ def login_is_required(function):
 
 @app.route('/login')
 def login():
-    session["google_id"] = "Test"
-    return redirect("/protected_area")
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
 
 @app.route('/callback')
 def callback():
-    pass
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500) #State does not match!
+    credentials = flow.credentials
+    request_session = request.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+    return id_info
+
 
 @app.route('/logout')
 def logout():
@@ -31,8 +66,9 @@ def logout():
 @app.route('/protected_area')
 @login_is_required
 def protected_area():
-    return "Protected Area! <a href='/logout'><button>Logout</button></a>"
+    return f"Hello {session['name']}! <br/> <a href='/logout'><button>Logout</button></a>"
 
+# Routes
 @app.route('/restaurants/error/<string:error>')
 def errorRestaurant(error):
     return render_template('error.html', error=error)
